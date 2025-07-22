7e124b1ef5600854057cba08d7f72a35af6246b6
Collins Abeiku Asamoah <collinsabeiku07@gmail.com>
2025-07-22 13:36:29 +0000

    Multiplication Table Generator

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,4 @@
 number = int(input("Enter a number to see its multiplication table: "))
-x = 1
 for x in range(1, 11):
     mul = number * x
-    print(number, "*", x, "=", mul)
+    print(f"{number} * {x} = {mul}")
