435abbeca7ba73d9ca5027a6ee9886b3c2ffb04d
Collins Abeiku Asamoah <collinsabeiku07@gmail.com>
2025-08-05 14:43:33 +0000

    add shopping list manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,36 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add_item = input("Enter the name of item: ")
+            shopping_list.append(add_item)
+            pass
+        elif choice == '2':
+            remove_item = input("Enter name of item to remove: ")
+            if remove_item in shopping_list:
+                shopping_list.remove(remove_item)
+            else:
+                print(f"{remove_item} is not in your shopping list")
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            print(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
