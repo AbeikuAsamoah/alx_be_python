deb538db8299d7edb27207a6cf1042afc7324f82
Collins Abeiku Asamoah <collinsabeiku07@gmail.com>
2025-09-12 11:54:09 +0000

    update explore_datetime file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -13,5 +13,5 @@ def calculate_future_date(n):
 
 
 display_current_datetime()
-n = int(input("Enter number of days to add to the current date: "))
+n = int(input("Enter the number of days to add to the current date: "))
 calculate_future_date(n)
